a02a48fef479e39bcc3789a7609d0fd228402a5e
iequivocality <xanthiandere@gmail.com>
2025-06-13 08:02:18 +0800

    japan

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router';
+import {
+  Japan
+} from "japan-prefectures-react";
+
 import '../App.css';
 
 export const Route = createFileRoute('/')({
@@ -6,5 +10,15 @@ export const Route = createFileRoute('/')({
 });
 
 function App() {
-  return <div className='App'></div>;
+  return <div className='App'>
+     <Japan
+        className="stroke-slate-400 fill-slate-100"
+        prefectureProps={{
+          className:
+            "transition-all stroke-slate-400 fill-white hover:fill-red-500",
+        }}
+        mapType='dense'
+      />
+
+  </div>;
 }
